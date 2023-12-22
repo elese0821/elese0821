f77a696bd67f8d29a47d87e06f8f731cde93f1e2
wonyoung <elese0821@naver.com>
2023-12-22 11:30:00 +0900

    D

---
+++ Desktop/react-blog/APP/sever/config/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://hoho:dlwm5656@cluster0.ctgdo3h.mongodb.net/ReactBlog?retryWrites=true&w=majority",
+    access_key: "cDwAr3A1UmGzO8COdrpD",
+    secret_key: "PI4mT4tqlH7DiArTwvCiNG5V4DqD4QepYV4hpGoD"
+}
+
+// mongodb+srv://hoho:dlwm5656@cluster0.ctgdo3h.mongodb.net/?retryWrites=true&w=majority
---
+++ Desktop/react-blog/APP/sever/config/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    access_key: process.env.S3_KEY,
+    secret_key: process.env.S3_SECRET,
+}
+
+// mongodb+srv://hoho:dlwm5656@cluster0.ctgdo3h.mongodb.net/?retryWrites=true&w=majority
